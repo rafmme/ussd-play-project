82eccf3a4c3b8571bd59c35263a852863f381e2c
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-01-25 18:05:10 +0100

    added sample dialer code

--- app/src/main/java/com/farayolatimileyin/banksussdcode/HomeActivity.java
+++ app/src/main/java/com/farayolatimileyin/banksussdcode/HomeActivity.java
@@ -1,16 +1,28 @@
 package com.farayolatimileyin.banksussdcode;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.ussdcode) EditText ussdText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,55 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick (R.id.button)
+    public void dial(){
+        String code = Uri.encode(ussdText.getText().toString());
+        if(Build.VERSION.SDK_INT < 23){
+            makeCall(code);
+        }
+        else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+                makeCall(code);
+            }
+            else{
+                final String[] PERMISSION_STORAGE = {Manifest.permission.CALL_PHONE};
+                ActivityCompat.requestPermissions(this,PERMISSION_STORAGE,9);
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean permissionGranted = false;
+        String code = Uri.encode(ussdText.getText().toString());
+        switch (requestCode){
+            case 9:
+                permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (permissionGranted){
+            makeCall(code);
+        }
+        else{
+            Toast.makeText(this,"Call Permission not granted",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void makeCall(String code){
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CALL_PHONE)==PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getApplicationContext(),"button clicked "+code,Toast.LENGTH_LONG).show();
+            Intent dialIntent = new Intent(Intent.ACTION_CALL);
+            dialIntent.setData(Uri.parse("tel:"+code));
+            this.startActivity(dialIntent);
+        }
+        else{
+            Toast.makeText(this,"Call Permission not granted",Toast.LENGTH_LONG).show();
+        }
 
     }
 
