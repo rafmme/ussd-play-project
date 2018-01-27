5472fee2163229738542da15c19d7d35974455e8
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-01-27 13:36:32 +0100

    load image in bankactivity layout

--- app/src/main/java/com/farayolatimileyin/banksussdcode/BankActionActivity.java
+++ app/src/main/java/com/farayolatimileyin/banksussdcode/BankActionActivity.java
@@ -3,18 +3,23 @@ package com.farayolatimileyin.banksussdcode;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BankActionActivity extends AppCompatActivity {
 
-    @BindView()
+    @BindView(R.id.bank_icon) ImageView bankImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_action);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
         String name = intent.getStringExtra("bankName");
         String bankIconName = intent.getStringExtra("bankImageName");
         getSupportActionBar().setTitle(name);
+        bankImage.setImageDrawable(HomeActivity.getDrawable(this,bankIconName));
     }
 }
