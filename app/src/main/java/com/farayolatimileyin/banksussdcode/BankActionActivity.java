12654507efe7924b892557f753dc332c3fe35489
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-01-27 19:40:44 +0100

    ...

--- app/src/main/java/com/farayolatimileyin/banksussdcode/BankActionActivity.java
+++ app/src/main/java/com/farayolatimileyin/banksussdcode/BankActionActivity.java
@@ -3,13 +3,22 @@ package com.farayolatimileyin.banksussdcode;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class BankActionActivity extends AppCompatActivity {
+public class BankActionActivity extends AppCompatActivity implements BankActionAdapter.BankActionGridItemClickListener {
 
     @BindView(R.id.bank_icon) ImageView bankImage;
+    RecyclerView rv_action;
+    RecyclerView.Adapter actionAdapter;
+    ArrayList<BankUssdData> actionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,21 @@ public class BankActionActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String name = intent.getStringExtra("bankName");
         String bankIconName = intent.getStringExtra("bankImageName");
+        actionList = intent.getParcelableArrayListExtra("bankussdlist");
         getSupportActionBar().setTitle(name);
         bankImage.setImageDrawable(HomeActivity.getDrawable(this,bankIconName));
+        loadRecylerView();
+    }
+
+    public void loadRecylerView(){
+        rv_action = (RecyclerView) findViewById(R.id.rv_bank_action);
+        rv_action.setLayoutManager(new LinearLayoutManager(this));
+        actionAdapter = new BankActionAdapter(this,actionList,this);
+        rv_action.setAdapter(actionAdapter);
+    }
+
+    @Override
+    public void onBankActionGridItemClickListener(int clickedItemIndex) {
+        Toast.makeText(this,actionList.get(clickedItemIndex).getUssdCode(),Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/farayolatimileyin/banksussdcode/BankActionAdapter.java
+++ app/src/main/java/com/farayolatimileyin/banksussdcode/BankActionAdapter.java
@@ -2,8 +2,11 @@ package com.farayolatimileyin.banksussdcode;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,30 +18,53 @@ public class BankActionAdapter extends RecyclerView.Adapter<BankActionAdapter.Ba
 
     private Context context;
     private ArrayList<BankUssdData> listOfBankUssdActions;
-    BankActionAdapter(Context context, ArrayList<BankUssdData>listOfBankUssdActions){
+    private final BankActionGridItemClickListener bankActionGridItemClickListener;
+
+    public BankActionAdapter(Context context, ArrayList<BankUssdData>listOfBankUssdActions, BankActionGridItemClickListener bankActionGridItemClickListener){
         this.context = context;
         this.listOfBankUssdActions = listOfBankUssdActions;
+        this.bankActionGridItemClickListener = bankActionGridItemClickListener;
     }
 
     @Override
     public void onBindViewHolder(BankActionViewHolder holder, int position) {
-
+        BankUssdData bankUssdData = listOfBankUssdActions.get(position);
+        holder.icon.setImageDrawable(HomeActivity.getDrawable(context,bankUssdData.getBankName()));
+        holder.action.setText(bankUssdData.getActionName());
+        holder.actionUssdCode.setText(bankUssdData.getUssdCode());
     }
 
     @Override
     public BankActionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = View.inflate();
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bank_ussd_action_layout,parent,false);
+        return new BankActionViewHolder(view);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listOfBankUssdActions.size();
     }
 
-    class BankActionViewHolder extends RecyclerView.ViewHolder{
+    public interface BankActionGridItemClickListener{
+        void onBankActionGridItemClickListener(int clickedItemIndex);
+    }
+
+    class BankActionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView icon;
+        TextView action;
+        TextView actionUssdCode;
         BankActionViewHolder(View view){
             super(view);
+            icon = (ImageView) view.findViewById(R.id.b_icon);
+            action = (TextView) view.findViewById(R.id.action_name);
+            actionUssdCode = (TextView) view.findViewById(R.id.action_ussd);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int clickedPosition = getAdapterPosition();
+            bankActionGridItemClickListener.onBankActionGridItemClickListener(clickedPosition);
         }
     }
 }
