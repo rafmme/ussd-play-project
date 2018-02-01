dc00733c4d65a1a398c92c48e3cd866e7e071c12
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-02-01 19:59:18 +0100

    .

--- app/src/main/java/com/farayolatimileyin/banksussdcode/PerformUssdTransactionActivity.java
+++ app/src/main/java/com/farayolatimileyin/banksussdcode/PerformUssdTransactionActivity.java
@@ -1,5 +1,6 @@
 package com.farayolatimileyin.banksussdcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,7 +9,9 @@ public class PerformUssdTransactionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.money_transfer_layout);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        String action = intent.getStringExtra("action");
+        setContentView(intent.getIntExtra("layout",R.layout.money_transfer_layout));
+        getSupportActionBar().setTitle(action);
     }
 }
